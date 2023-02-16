5184755b95dded8f28aaa38882d3c1ca646405e2
Garrett <gwillis501@gmail.com>
2023-02-16 11:25:16 -0800

    added battle class

--- src/Battle.java
+++ src/Battle.java
@@ -4,7 +4,53 @@ image or sprite, try to save it as we may use it in animations.
  */
 public class Battle {
 
-    public void battleStart(){
-        
+    Enemy opponent;
+
+    public void battleStart( int enemyDam){
+        boolean fight = true;
+
+        while(fight) {
+            playerAttack(); //This needs to assigned to a button in javafx
+            //methods are called for testing
+
+            if(Main.character.playHP == 0 && opponent.HP == 0){
+                fight = endBattle();
+            }
+
+            enemyAttack();
+
+            if(Main.character.playHP == 0 && opponent.HP == 0){
+                fight = endBattle();
+            }
+
+
+        }
+    }
+
+    public void enemyAttack(){
+        int[] damArray = opponent.calDamage();
+        Main.character.changeHP(damArray[0]);
+
+    }
+
+    public void playerAttack(){
+        int[] damArray = Main.character.calDamage();
+
+        for(int n = 0; n != damArray.length; n++ ){
+            opponent.changeHP(damArray[n]); //Needs to update health observer/ health bar
+        }
+
+    }
+
+    public boolean endBattle() {
+    if(Main.character.playHP == 0){
+        return false;
+        }else if(opponent.HP == 0){
+        return false;
+        }else {
+        System.out.println("Error endBattle() was incorrectly called.");
+        return false;
+    }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 
+    static Player character = new Player();
+
     public static int ranCal(int min, int max){  //This is just to easily find a random int between a min and max
         int ranDam = (int)((Math.random() * ((max+1) - min)) + min);//number without having to type it all out.
         return ranDam;
