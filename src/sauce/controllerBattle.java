55a8d53960e239d70aaeaf65f4fac70c2dc2a49b
Garrett <gwillis501@gmail.com>
2023-03-07 12:53:12 -0800

    added to battle controller and updated battle scene

--- src/sauce/controllerBattle.java
+++ src/sauce/controllerBattle.java
@@ -1,4 +1,29 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 public class controllerBattle {
+    private controllerGame gameControl;
+    private appDungGame app;
+    @FXML
+    private Rectangle healthBar;
+    @FXML
+    private Rectangle manaBar;
+    @FXML
+    private Rectangle player;
+    @FXML
+    private Rectangle enemy;
+
+
+    public void setApp(appDungGame app,controllerGame gameControl){
+        /*This method sets the app variable to appDungGame application so the window can be changed.
+        And sets game control to the dungeonGame.fxml controller, so it can be altered from this controller.*/
+        this.app = app;
+        this.gameControl = gameControl;
+    }
 
 
 }
